c12b031977f5ff425638b704d753cfc274f3af9c
Neel <isioza0@gmail.com>
2026-03-26 11:17:08 +0530

    chore: Added Env based properties

---
+++ src/test/java/com/tss/LoanEmiScheduler/LoanEmiSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tss.LoanEmiScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEmiSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
