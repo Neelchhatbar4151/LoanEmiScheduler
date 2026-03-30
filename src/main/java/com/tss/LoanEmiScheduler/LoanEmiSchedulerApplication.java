039e01335c1c1337f426d72655f9b1509e1a7923
Neel.Chhantbar <neel.chhantbar@tssconsultancy.com>
2026-03-30 16:40:25 +0530

    feat: Email Notification Service Implemented

--- src/main/java/com/tss/LoanEmiScheduler/LoanEmiSchedulerApplication.java
+++ src/main/java/com/tss/LoanEmiScheduler/LoanEmiSchedulerApplication.java
@@ -3,18 +3,22 @@ package com.tss.LoanEmiScheduler;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class LoanEmiSchedulerApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Dotenv dotenv = Dotenv.load();
 
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
-		SpringApplication.run(LoanEmiSchedulerApplication.class, args);
+		System.setProperty("EMAIL", dotenv.get("EMAIL"));
+		System.setProperty("EMAIL_APP_PASSWORD", dotenv.get("EMAIL_APP_PASSWORD"));
+
+		ConfigurableApplicationContext context = SpringApplication.run(LoanEmiSchedulerApplication.class, args);
 	}
 }
---
+++ src/test/java/com/tss/LoanEmiScheduler/service/EmailNotificationServiceTest.java
@@ -0,0 +1,22 @@
+package com.tss.LoanEmiScheduler.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EmailNotificationServiceTest {
+    @Autowired
+    private EmailNotificationService emailService;
+    @Test
+    void sendNotification() throws Exception {
+
+        emailService.sendNotification(
+                "neel.chhantbar@tssconsultancy.com",
+                "Test Email",
+                "Hello from Spring Boot!"
+        );
+    }
+}
