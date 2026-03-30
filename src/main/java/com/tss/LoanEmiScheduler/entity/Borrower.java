dab5a82475de9e865618247029998ee3872361e4
Bhavika Chhatbar <bhavika.chhatbar@tssconsultancy.com>
2026-03-30 11:12:15 +0530

    feat: add branch into borrower entity

--- src/main/java/com/tss/LoanEmiScheduler/entity/Borrower.java
+++ src/main/java/com/tss/LoanEmiScheduler/entity/Borrower.java
@@ -1,9 +1,6 @@
 package com.tss.LoanEmiScheduler.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -40,4 +37,8 @@ public class Borrower extends User{
     @Column(nullable = false)
     @ColumnDefault("false")
     private Boolean accountLocked = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "branch_id", nullable = false)
+    private Branch branch;
 }
