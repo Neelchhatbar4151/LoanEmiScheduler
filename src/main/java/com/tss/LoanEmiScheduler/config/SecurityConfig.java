0fe2368364576cb8faefc003ce577f376b1f6afa
Bhavika Chhatbar <bhavikachhatbar2004@gmail.com>
2026-04-01 01:25:37 +0530

    refactor: replace deprecated method 'setUserDetailsService' from authenticationProvider with constructor

--- src/main/java/com/tss/LoanEmiScheduler/config/SecurityConfig.java
+++ src/main/java/com/tss/LoanEmiScheduler/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.tss.LoanEmiScheduler.config;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.tss.LoanEmiScheduler.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,10 +14,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -49,9 +45,8 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
         provider.setPasswordEncoder(new BCryptPasswordEncoder(6));
-        provider.setUserDetailsService(userDetailsService);
         return provider;
     }
 
